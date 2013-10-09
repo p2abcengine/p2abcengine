188c12cad336b89d9deb134e1c32a49ff16a5796
Franz-Stefan Preiss <frp@zurich.ibm.com>
2013-10-09 11:12:26 +0200

    Update web-service tutorial description and scripts.

--- Code/core-abce/abce-services/setupTutorial.sh
+++ Code/core-abce/abce-services/setupTutorial.sh
@@ -3,16 +3,12 @@
 #Stop script if an error occurs
 set -e
 
-rm -f issuerParameters.xml
-rm -f systemparameters.xml
-rm -f revocationAuthorityParameters.xml
-rm -f issuanceMessageAndBoolean.xml
-
 rm -rf target/user_*
 rm -rf tmp/issuer/issuer_*
 rm -rf tmp/verifier/abce-services
 rm -rf tmp/revocation/revocation_storage
 rm -rf tmp/user/user_storage
+rm -rf tmp/user/abce-services
 rm -rf tmp/*.xml
 
 mkdir -p tmp
@@ -29,3 +25,4 @@ cp target/selfcontained-user-service.war tmp/user
 cp target/selfcontained-verification-service.war tmp/verifier
 cp target/selfcontained-inspection-service.war tmp/inspector
 cp target/selfcontained-revocation-service.war tmp/revocation
+cp tutorial.sh tmp/
