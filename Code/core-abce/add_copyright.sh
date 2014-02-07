45c340353a306a568884c762da9841625c4d14eb
Robert R. Enderlein <enr@zurich.ibm.com>
2014-02-07 16:36:40 +0100

    Updated copyright script.

--- Code/core-abce/add_copyright.sh
+++ Code/core-abce/add_copyright.sh
@@ -38,7 +38,7 @@ if grep "microsoft" "$AUTH" -q; then
   cat - > "$TEMP" <<END
 //* Licensed Materials - Property of IBM, Miracle A/S,                *
 //* Alexandra Instituttet A/S, and Microsoft                          *
-//* eu.abc4trust.pabce.1.0                                            *
+//* eu.abc4trust.pabce.1.14                                           *
 //* (C) Copyright IBM Corp. 2012. All Rights Reserved.                *
 //* (C) Copyright Miracle A/S, Denmark. 2012. All Rights Reserved.    *
 //* (C) Copyright Alexandra Instituttet A/S, Denmark. 2012. All       *
@@ -46,6 +46,17 @@ if grep "microsoft" "$AUTH" -q; then
 //* (C) Copyright Microsoft Corp. 2012. All Rights Reserved.          *
 //* US Government Users Restricted Rights - Use, duplication or       *
 //* disclosure restricted by GSA ADP Schedule Contract with IBM Corp. *
+//*                                                                   *
+//* This file is licensed under the Apache License, Version 2.0 (the  *
+//* "License"); you may not use this file except in compliance with   *
+//* the License. You may obtain a copy of the License at:             *
+//*   http://www.apache.org/licenses/LICENSE-2.0                      *
+//* Unless required by applicable law or agreed to in writing,        *
+//* software distributed under the License is distributed on an       *
+//* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY            *
+//* KIND, either express or implied.  See the License for the         *
+//* specific language governing permissions and limitations           *
+//* under the License.                                                *
 //*/**/****************************************************************
 
 END
@@ -54,13 +65,24 @@ elif grep -v "zurich.ibm.com" "$AUTH" -q; then
   cat - > "$TEMP" <<END
 //* Licensed Materials - Property of IBM, Miracle A/S, and            *
 //* Alexandra Instituttet A/S                                         *
-//* eu.abc4trust.pabce.1.0                                            *
+//* eu.abc4trust.pabce.1.14                                           *
 //* (C) Copyright IBM Corp. 2012. All Rights Reserved.                *
 //* (C) Copyright Miracle A/S, Denmark. 2012. All Rights Reserved.    *
 //* (C) Copyright Alexandra Instituttet A/S, Denmark. 2012. All       *
 //* Rights Reserved.                                                  *
 //* US Government Users Restricted Rights - Use, duplication or       *
 //* disclosure restricted by GSA ADP Schedule Contract with IBM Corp. *
+//*                                                                   *
+//* This file is licensed under the Apache License, Version 2.0 (the  *
+//* "License"); you may not use this file except in compliance with   *
+//* the License. You may obtain a copy of the License at:             *
+//*   http://www.apache.org/licenses/LICENSE-2.0                      *
+//* Unless required by applicable law or agreed to in writing,        *
+//* software distributed under the License is distributed on an       *
+//* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY            *
+//* KIND, either express or implied.  See the License for the         *
+//* specific language governing permissions and limitations           *
+//* under the License.                                                *
 //*/**/****************************************************************
 
 END
@@ -68,10 +90,21 @@ else
   echo "IBM $1";
   cat - > "$TEMP" <<END
 //* Licensed Materials - Property of IBM                              *
-//* eu.abc4trust.pabce.1.0                                            *
+//* eu.abc4trust.pabce.1.14                                           *
 //* (C) Copyright IBM Corp. 2012. All Rights Reserved.                *
 //* US Government Users Restricted Rights - Use, duplication or       *
 //* disclosure restricted by GSA ADP Schedule Contract with IBM Corp. *
+//*                                                                   *
+//* This file is licensed under the Apache License, Version 2.0 (the  *
+//* "License"); you may not use this file except in compliance with   *
+//* the License. You may obtain a copy of the License at:             *
+//*   http://www.apache.org/licenses/LICENSE-2.0                      *
+//* Unless required by applicable law or agreed to in writing,        *
+//* software distributed under the License is distributed on an       *
+//* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY            *
+//* KIND, either express or implied.  See the License for the         *
+//* specific language governing permissions and limitations           *
+//* under the License.                                                *
 //*/**/****************************************************************
 
 END
