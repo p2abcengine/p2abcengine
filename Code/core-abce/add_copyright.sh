93e193ab6b8f30c601aa199a0488a69bc21d98ba
Robert R. Enderlein <enr@zurich.ibm.com>
2014-02-07 16:43:35 +0100

    Added Apache License to copyright header.

--- Code/core-abce/add_copyright.sh
+++ Code/core-abce/add_copyright.sh
@@ -4,6 +4,17 @@
 # (C) Copyright IBM Corp. 2012. All Rights Reserved.                *
 # US Government Users Restricted Rights - Use, duplication or       *
 # disclosure restricted by GSA ADP Schedule Contract with IBM Corp. *
+#                                                                   *
+# This file is licensed under the Apache License, Version 2.0 (the  *
+# "License"); you may not use this file except in compliance with   *
+# the License. You may obtain a copy of the License at:             *
+#   http://www.apache.org/licenses/LICENSE-2.0                      *
+# Unless required by applicable law or agreed to in writing,        *
+# software distributed under the License is distributed on an       *
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY            *
+# KIND, either express or implied.  See the License for the         *
+# specific language governing permissions and limitations           *
+# under the License.                                                *
 #********************************************************************
 
 # Example usage:
@@ -38,7 +49,7 @@ if grep "microsoft" "$AUTH" -q; then
   cat - > "$TEMP" <<END
 //* Licensed Materials - Property of IBM, Miracle A/S,                *
 //* Alexandra Instituttet A/S, and Microsoft                          *
-//* eu.abc4trust.pabce.1.14                                           *
+//* eu.abc4trust.pabce.1.0                                            *
 //* (C) Copyright IBM Corp. 2012. All Rights Reserved.                *
 //* (C) Copyright Miracle A/S, Denmark. 2012. All Rights Reserved.    *
 //* (C) Copyright Alexandra Instituttet A/S, Denmark. 2012. All       *
@@ -65,7 +76,7 @@ elif grep -v "zurich.ibm.com" "$AUTH" -q; then
   cat - > "$TEMP" <<END
 //* Licensed Materials - Property of IBM, Miracle A/S, and            *
 //* Alexandra Instituttet A/S                                         *
-//* eu.abc4trust.pabce.1.14                                           *
+//* eu.abc4trust.pabce.1.0                                            *
 //* (C) Copyright IBM Corp. 2012. All Rights Reserved.                *
 //* (C) Copyright Miracle A/S, Denmark. 2012. All Rights Reserved.    *
 //* (C) Copyright Alexandra Instituttet A/S, Denmark. 2012. All       *
@@ -90,7 +101,7 @@ else
   echo "IBM $1";
   cat - > "$TEMP" <<END
 //* Licensed Materials - Property of IBM                              *
-//* eu.abc4trust.pabce.1.14                                           *
+//* eu.abc4trust.pabce.1.0                                            *
 //* (C) Copyright IBM Corp. 2012. All Rights Reserved.                *
 //* US Government Users Restricted Rights - Use, duplication or       *
 //* disclosure restricted by GSA ADP Schedule Contract with IBM Corp. *
